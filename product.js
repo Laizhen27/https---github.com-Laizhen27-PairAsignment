bfe07c51777b8f65747a44865214d034344dc4d7
Wen Bin Koh <s10242132@connect.np.edu.sg>
2023-02-08 15:43:23 +0800

    added hyperlink to shirt

---
+++ product.js
@@ -0,0 +1,11 @@
+let settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://idasg-39a1.restdb.io/rest/shirt",
+    "method": "GET",
+    "headers": {
+      "content-type": "application/json",
+      "x-apikey": "63d641823bc6b255ed0c43e4",
+      "cache-control": "no-cache"
+    }
+}
